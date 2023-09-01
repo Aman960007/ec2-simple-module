8e0d0b8ebe363b9a6f92198be58fcd08c07772a5
Aman960007 <amandeep3095@gmail.com>
2023-09-01 16:21:11 +0530

    Update variable.tf

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "instance_type" {
+  description = "Name of the instance"
+  type        = string
+  default     = ""
+}
+
+variable "ami" {
+  description = "name of the ami"
+  type        = string
+  default     = ""
+}
